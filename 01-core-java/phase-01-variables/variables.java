56a7be26c3f28d6f4f114f51244b16ec0a8c8117
harshvardhanmishra492@gmail.com <harshvardhanmishra492@gmail.com>
2026-08-16 23:58:18 +0530

    Data Types

---
+++ 01-core-java/phase-01-variables/variables.java
@@ -0,0 +1,12 @@
+class variables{
+    public static void main (String args[])
+    {
+        System.out.print("Data Type Demonstration");
+        int a=10;
+        float b=20.5f;
+        double c=30.5;
+        System.out.println("Integer value: " + a);
+        System.out.println("Float value: " + b);
+        System.out.println("Double value: " + c);
+    }
+}
