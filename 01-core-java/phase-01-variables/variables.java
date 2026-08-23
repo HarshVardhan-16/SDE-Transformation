5ffbdfbe1a6ccdd23df714de502651959e3a77a2
Harsh Vardhan Mishra <harshvardhanmishra492@gmail.com>
2026-08-23 23:20:33 +0530

    Learnt Data Types

--- 01-core-java/phase-01-variables/variables.java
+++ 01-core-java/phase-01-variables/variables.java
@@ -5,8 +5,17 @@ class variables{
         int a=10;
         float b=20.5f;
         double c=30.5;
+        long f =  372837;
+        char e = 'a';
+        byte test = 2;
+        boolean g = true;
+
         System.out.println("Integer value: " + a);
         System.out.println("Float value: " + b);
         System.out.println("Double value: " + c);
+        System.out.println("Long value: " + f);
+        System.out.println("char value: " + e);
+        System.out.println("byte value: " + test);
+        System.out.println("boolean value: " + g);
     }
 }
